263318faffbae0a8c031f7eb6b96cc81ad9a7313
rehansajjadsiyal <princerehan6632@gmail.com>
2019-07-11 06:52:05 -0400

    finishing sing-up branch

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,22 @@
 class UsersController < ApplicationController
   def show
-    @users=User.find(params[:id])
+    @user=User.find(params[:id])
     debugger
   end
   def new
-    @users=User.new
+    @user=User.new
+  end
+  def create
+    @user=User.new(user_params)
+    if @user.save
+      flash[:Sucess] = "Welcome to Simple App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
   end
 end
